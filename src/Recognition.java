925a4761c4127546144ee74160fd432f8d5d809c
i302649 <zephyr.du@sap.com>
2015-03-18 18:12:41 +0800

    Initial

---
+++ src/Recognition.java
@@ -0,0 +1,22 @@
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+
+public class Recognition {
+
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File f = new File(".jpg");
+		BufferedImage bi = ImageIO.read(f);
+//		bi.getRG
+		java.util.Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("jpg");
+		ImageReader reader = (ImageReader)readers.next();
+		
+	}
+
+}
