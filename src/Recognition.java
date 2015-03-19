47d70b9e16a7381cbb410ddd4f242c0b6c9822fe
i302649 <zephyr.du@sap.com>
2015-03-19 18:15:27 +0800

    Update

--- src/Recognition.java
+++ src/Recognition.java
@@ -1,5 +1,6 @@
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.*;
 
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
@@ -11,12 +12,17 @@ public class Recognition {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		File f = new File(".jpg");
-		BufferedImage bi = ImageIO.read(f);
-//		bi.getRG
-		java.util.Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("jpg");
-		ImageReader reader = (ImageReader)readers.next();
+//		File f = new File(".jpg");
+//		BufferedImage bi = ImageIO.read(f);
+////		bi.getRG
+//		java.util.Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("jpg");
+//		ImageReader reader = (ImageReader)readers.next();
+//		
+		
+		
 		
 	}
+	
+	
 
 }
